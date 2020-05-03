e8e6368fb71c56af82d616eb0cd3d8b0d69b9e3e
Kyuunex <kyuunex@protonmail.ch>
2020-05-03 11:48:24 +0400

    version 1.0.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='osuembed',
     packages=['osuembed'],
-    version='1.0.0',
+    version='1.0.1',
     description='osuembed',
     author='Kyuunex',
     url='https://github.com/Kyuunex/osudiscordpyembed',
-    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/1.0.0',
+    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/1.0.1',
     keywords=['osu', 'discord'],
     classifiers=[],
     requires=['pycountry', 'discord.py'],
