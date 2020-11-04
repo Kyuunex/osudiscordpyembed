d33a9a8ec35f5b8d63609dd0ac99fdc6cdfdffb7
Kyuunex <kyuunex@protonmail.ch>
2020-11-04 17:21:43 +0400

    bump version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='osuwebembed',
     packages=['osuwebembed'],
-    version='2.0.0-placeholder.4',
+    version='2.0.0-placeholder.5',
     description='osuwebembed',
     author='Kyuunex',
     url='https://github.com/Kyuunex/osudiscordpyembed',
-    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/2.0.0-placeholder.4',
+    download_url='https://github.com/Kyuunex/osudiscordpyembed/tarball/2.0.0-placeholder.5',
     keywords=['osu', 'discord'],
     classifiers=[],
     requires=['discord.py'],
